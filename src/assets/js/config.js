c5486c9c9a41d9cea6ee4b615b71b1ed82c3d0e9
Mourisse Dieter <dieter.mourisse@howest.be>
2025-03-20 16:38:43 +0000

    Remove trailing slash

--- src/assets/js/config.js
+++ src/assets/js/config.js
@@ -4,7 +4,7 @@ const GROUPTOKEN = "TOKEN";
 const ERRORHANDLERSELECTOR = ".errormessages p";
 
 const LOCALSERVER = `http://localhost:8001`;
-const DEPLOYEDSERVER = `https://project-1.ti.howest.be/2024-2025/splendor/api/`;
+const DEPLOYEDSERVER = `https://project-1.ti.howest.be/2024-2025/splendor/api`;
 const GROUPDEPLOYEDSERVER = `https://project-1.ti.howest.be/2024-2025/group-${GROUPNUMBER}/api`;
 
 function getAPIUrl() {
