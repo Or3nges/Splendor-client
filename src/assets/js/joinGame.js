050f3b3f24f782893bdf6864c9053fc55e7f966f
Sebastiaan <sebastiaan.vermeulen@student.howest.be>
2025-03-25 11:15:42 +0100

    Added way to return to index page

---
+++ src/assets/js/joinGame.js
@@ -0,0 +1,9 @@
+function init() {
+    console.log("Join Game Page");
+}
+
+document.querySelector("#backButton").addEventListener("click", () => {
+    window.location.href = "../index.html";
+});
+
+init();
