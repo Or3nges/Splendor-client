2bbdda13ce1726aef31694d072b57a52b6faf8cd
Lohani Pratik <pratik.lohani@student.howest.be>
2025-03-27 12:10:54 +0100

    Show players in lobby works with API + css done + button functionality + players dynamic loaded

---
+++ src/assets/js/lobby.js
@@ -0,0 +1,58 @@
+import * as CommunicationAbstractor from "./data-connector/api-communication-abstractor.js";
+import * as StorageAbstractor from "./data-connector/local-storage-abstractor.js";
+
+const TIMEOUT = 5000;
+
+document.querySelector("#backButton").addEventListener("click", () => {
+    window.location.href = "../index.html";
+});
+
+document.querySelector("#startGameButton").addEventListener("click", () => {
+    window.location.href = "../html/gamescreen.html";
+});
+
+function fetchGameLobby() {
+    const gameId = StorageAbstractor.loadFromStorage("gameId");
+    if (!gameId) {
+        console.error("No gameId found in storage.");
+        return;
+    }
+
+    CommunicationAbstractor.fetchFromServer("/games", "GET")
+        .then(data => {
+            const games = data.games;
+
+            const currentGame = games.find(game => game.gameId === gameId);
+            if (currentGame) {
+                displayLobby(currentGame);
+            } else {
+                console.error("Game with the specified gameId not found.");
+            }
+        })
+        .catch(error => {
+            console.error("Error fetching games:", error);
+        });
+}
+
+
+function displayLobby(game) {
+    const main = document.querySelector("main");
+    main.innerHTML = "";
+
+
+    const header = document.createElement("h2");
+    header.innerText = `Game Lobby: ${game.gameName ? game.gameName : "Unnamed Game"}`;
+    main.appendChild(header);
+
+        const ul = document.createElement("ul");
+        game.players.forEach(player => {
+            const li = document.createElement("li");
+            li.innerText = player;
+            ul.appendChild(li);
+        });
+        main.appendChild(ul);
+    }
+
+
+fetchGameLobby();
+setInterval(fetchGameLobby, TIMEOUT);
