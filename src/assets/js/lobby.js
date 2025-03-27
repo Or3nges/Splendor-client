9c15216013a26a3419769e72051d2578780148ee
Lohani Pratik <pratik.lohani@student.howest.be>
2025-03-27 13:57:28 +0100

    Clear everything but name from localstorage when leaving game

--- src/assets/js/lobby.js
+++ src/assets/js/lobby.js
@@ -4,6 +4,7 @@ import * as StorageAbstractor from "./data-connector/local-storage-abstractor.js
 const TIMEOUT = 5000;
 
 document.querySelector("#backButton").addEventListener("click", () => {
+    StorageAbstractor.deleteFromStorage("gameId", "gameName", "numberOfPlayers");
     window.location.href = "../index.html";
 });
 
