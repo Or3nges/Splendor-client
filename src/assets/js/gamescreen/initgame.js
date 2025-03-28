025e7cbe3416dc90b98b32467973c5d6c60d315f
Wouter <wouter.taris@student.howest.be>
2025-03-28 11:51:37 +0100

    buy or reserve option complete + file name changes

--- src/assets/js/gamescreen/initgame.js
+++ src/assets/js/gamescreen/initgame.js
@@ -1,5 +1,5 @@
 import {fetchDevelopmentCards} from "../gamescreen/rendercards.js";
-import {initPopup} from "./end-game.js";
+import {initPopup} from "./popup.js";
 import { fetchPlayers} from "../gamescreen/player.js";
 
 function initGame() {
