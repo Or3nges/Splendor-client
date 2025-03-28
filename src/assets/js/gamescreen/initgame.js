6cd7f6dd335f6389ae3901edb585343252eb6a1e
Wouter <wouter.taris@student.howest.be>
2025-03-28 15:27:43 +0100

    leave game popup and link between buy-or-reserve-option and buy-cards-option

--- src/assets/js/gamescreen/initgame.js
+++ src/assets/js/gamescreen/initgame.js
@@ -1,11 +1,12 @@
 import {fetchDevelopmentCards} from "../gamescreen/rendercards.js";
-import {initPopup} from "./popup.js";
+import {initPopup, initBuyOption} from "./popup.js";
 import { fetchPlayers} from "../gamescreen/player.js";
 
 function initGame() {
    fetchDevelopmentCards();
   initPopup();
     fetchPlayers();
+    initBuyOption()
 }
 
 initGame();
