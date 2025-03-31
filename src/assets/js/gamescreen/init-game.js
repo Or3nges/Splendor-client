ba26216d9c779d47d43c17fa2eebe30dfa88d90b
Van Mulders Michiel <michiel.van.mulders@student.howest.be>
2025-03-31 15:08:16 +0200

    dynamically loaded the unclaimed nobles

--- src/assets/js/gamescreen/init-game.js
+++ src/assets/js/gamescreen/init-game.js
@@ -2,12 +2,14 @@ import {fetchDevelopmentCards} from "./render-cards.js";
 import {fetchGems} from "./player.js";
 import {initTurnIndication} from "./turn-indication.js";
 import {initPopup} from "./popup.js";
+import {initNobles} from "./nobles.js";
 
 function initGame() {
    fetchDevelopmentCards();
    initPopup();
    fetchGems();
    initTurnIndication();
+   initNobles()
 }
 
 initGame();
---
+++ src/assets/js/gamescreen/nobles.js
@@ -0,0 +1,45 @@
+import * as StorageAbstractor from "../data-connector/local-storage-abstractor.js";
+import * as CommunicationAbstractor from "../data-connector/api-communication-abstractor.js";
+import {createLiElement} from "../util.js";
+import {allGems} from "../Objects/gems.js";
+
+const gameId = StorageAbstractor.loadFromStorage("gameId");
+
+function initNobles() {
+    fetchUnclaimedNobles();
+}
+
+function fetchUnclaimedNobles() {
+    CommunicationAbstractor.fetchFromServer(`/games/${gameId}`)
+        .then(data => data.unclaimedNobles.forEach(noble => renderNoble(noble)));
+}
+
+
+
+
+function renderNoble(noble) {
+    console.log(noble)
+    const $template = document.querySelector("template#noble-template");
+    const $section = document.querySelector("section#allNobles")
+    const playerClone = $template.content.firstElementChild.cloneNode(true);
+
+    const nobleCardImage = playerClone.querySelector("img");
+    const nobleCostUl = playerClone.querySelector("ul");
+    const prestigeP = playerClone.querySelector("p")
+
+    nobleCardImage.setAttribute("src", "assets/images/nobleDevelopmentCard.png");
+    nobleCardImage.setAttribute("alt", `${noble.name}`);
+    nobleCardImage.setAttribute("title", `${noble.name}`);
+    Object.keys(noble.neededBonuses).forEach(bonusCost => {nobleCostUl.insertAdjacentHTML('beforeend', createLiElement(noble.neededBonuses[bonusCost], findGem(bonusCost).cardId))});
+    const p = `<p>${noble.prestigePoints}</p>`;
+    if (noble.prestigePoints !== 0) {
+        playerClone.insertAdjacentHTML('beforeend', p);
+    }
+    $section.insertAdjacentHTML('beforeend', playerClone.outerHTML);
+}
+
+function findGem(gemName) {
+    return allGems.filter(gem => gem.name === gemName)[0];
+}
+
+export {initNobles}
