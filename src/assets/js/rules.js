9ee7d4dbce37dd8fab6ee551b452cb7accb96fd0
Sebastiaan <sebastiaan.vermeulen@student.howest.be>
2025-04-01 15:11:14 +0200

    Changed rules.js to be easier to read

--- src/assets/js/rules.js
+++ src/assets/js/rules.js
@@ -9,10 +9,14 @@ document.querySelector("#back-button").addEventListener("click", () => {
     window.location.href = "../index.html";
 });
 
-
 function showArticle(index) {
+    articles.forEach(article => article.classList.remove('active'));
+    articles[index].classList.add('active');
+}
+
+function highlightCurrentArticle() {
     articles.forEach((article, i) => {
-        article.classList.toggle('active', i === index);
+        article.classList.toggle('highlight', i === currentArticleIndex);
     });
 }
 
@@ -22,11 +26,13 @@ const nextButton = document.querySelector('.nav-buttons button:nth-child(2)');
 prevButton.addEventListener('click', () => {
     currentArticleIndex = (currentArticleIndex > 0) ? currentArticleIndex - 1 : articles.length - 1;
     showArticle(currentArticleIndex);
+    highlightCurrentArticle();
 });
 
 nextButton.addEventListener('click', () => {
     currentArticleIndex = (currentArticleIndex < articles.length - 1) ? currentArticleIndex + 1 : 0;
     showArticle(currentArticleIndex);
+    highlightCurrentArticle();
 });
 
 init();
