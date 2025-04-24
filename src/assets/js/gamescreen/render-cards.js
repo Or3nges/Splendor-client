0810f3796dc5fb3bcf959d7de861deeb8e129149
Sebastiaan Vermeulen <sebastiaan.vermeulen@student.howest.be>
2025-04-24 18:58:00 +0200

    made purchase popup show up when card is selected and converted px to rem

--- src/assets/js/gamescreen/render-cards.js
+++ src/assets/js/gamescreen/render-cards.js
@@ -20,25 +20,48 @@ function findCard(tier) {
     return allDevelopmentCards.filter(tierLevel => tier === tierLevel.level)[0];
 }
 
-function displayDevelopmentCards(card) {
-    const $gamescreenArticle = document.querySelector("#gameScreen div");
-    const $template = document.querySelector('#developmentCard').content.firstElementChild.cloneNode(true);
-    const $cost = $template.querySelector('#cost');
-
-    const p = `<p>${card.prestigePoints}</p>`;
-    if (card.prestigePoints !== 0) {
-        $template.insertAdjacentHTML('beforeend', p);
-    }
-
-    Object.keys(card.cost).forEach(bonusCost => {$cost.insertAdjacentHTML('beforeend', createLiElement(card.cost[bonusCost], findGemByName(bonusCost).tokenId))});
-    $template.querySelector('#cardType').setAttribute('src', findCard(card.level).img);
-    $template.querySelector('#cardToken').setAttribute('src', findGemByName(card.bonus).img);
-    $template.addEventListener('click', () => {
+function addCardEventListeners($template, $popup) {
+    $template.addEventListener('click', async () => {
         document.querySelectorAll("#gameScreen div figure").forEach(cardElement => {
             cardElement.classList.remove("selected");
         });
         $template.classList.add("selected");
+        $popup.style.display = "block";
     });
+
+    const $closeButton = $popup.querySelector('.close');
+    $closeButton.addEventListener('click', () => {
+        document.querySelectorAll("#gameScreen div figure").forEach(cardElement => {
+            cardElement.classList.remove("selected");
+        });
+        $popup.style.display = "none";
+    });
+}
+
+function populateCardDetails($template, card) {
+    const $cost = $template.querySelector('#cost');
+    Object.keys(card.cost).forEach(bonusCost => {
+        $cost.insertAdjacentHTML('beforeend', createLiElement(card.cost[bonusCost], findGemByName(bonusCost).tokenId));
+    });
+    $template.querySelector('#cardType').setAttribute('src', findCard(card.level).img);
+    $template.querySelector('#cardToken').setAttribute('src', findGemByName(card.bonus).img);
+}
+
+function createCardTemplate(card) {
+    const $template = document.querySelector('#developmentCard').content.firstElementChild.cloneNode(true);
+    if (card.prestigePoints !== 0) {
+        $template.insertAdjacentHTML('beforeend', `<p>${card.prestigePoints}</p>`);
+    }
+    return $template;
+}
+
+function displayDevelopmentCards(card) {
+    const $gamescreenArticle = document.querySelector("#gameScreen div");
+    const $popup = document.querySelector("#buy-or-reserve-option");
+
+    const $template = createCardTemplate(card);
+    populateCardDetails($template, card);
+    addCardEventListeners($template, $popup);
 
     $gamescreenArticle.appendChild($template);
 }
