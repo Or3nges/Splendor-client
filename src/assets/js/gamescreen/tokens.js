957be84baac2182f392fe53b42e6e8eb6e62db59
Lohani Pratik <pratik.lohani@student.howest.be>
2025-04-27 21:33:53 +0200

    Remove useless check for gameId and playerName validation

--- src/assets/js/gamescreen/tokens.js
+++ src/assets/js/gamescreen/tokens.js
@@ -13,8 +13,6 @@ function isCurrentPlayerTurn() {
     const gameId = loadFromStorage("gameId");
     const playerName = loadFromStorage("playerName");
 
-    if (!gameId || !playerName) return Promise.resolve(false);
-
     return fetchGame(gameId)
         .then(game => {
             return game.currentPlayer === playerName;
