9cb4584035b4080a47aed0f9fcf9137d287532d8
Lohani Pratik <pratik.lohani@student.howest.be>
2025-04-27 20:30:01 +0200

    getCurrentPlayerToken and retrieveToken done

--- src/assets/js/gamescreen/tokens.js
+++ src/assets/js/gamescreen/tokens.js
@@ -1,7 +1,13 @@
-import {fetchGame} from "../util.js";
-
+import {fetchGame, createLiElement, findGemByName, findGemByTokenId} from "../util.js";
+import {fetchFromServer} from "../data-connector/api-communication-abstractor.js";
 import {loadFromStorage} from "../data-connector/local-storage-abstractor.js";
+import { fetchPlayers } from './player.js';
 
+let selectedTokens = {};
+let availableTokens = {};
+let isMyTurn = false;
+
+const TOKEN_SELECTED_MAX = 3;
 
 function isCurrentPlayerTurn() {
     const gameId = loadFromStorage("gameId");
@@ -15,6 +21,24 @@ function isCurrentPlayerTurn() {
         })
         .catch(() => {
             return false;
+        });
+}
+
+function getCurrentPlayerTokens(gameId, playerName) {
+    return fetchGame(gameId)
+        .then(gameData => gameData.players.find(p => p.name === playerName)?.tokens ?? null) // moet optional chaining gebruiken anders geeft sonarqube een issue
+        .catch(() => null);
+}
+
+
+function retrieveTokens(gameId) {
+    fetchGame(gameId)
+        .then(data => {
+            availableTokens = {...data.unclaimedTokens};
+
+            isMyTurn = data.currentPlayer === loadFromStorage("playerName");
+
+            displayTokens(data.unclaimedTokens);
         });
 }
 
