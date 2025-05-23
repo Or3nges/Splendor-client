33f6167cc4062f6115ca111514f858726e75b7bc
Wouter <wouter.taris@student.howest.be>
2025-05-23 11:39:02 +0200

    first draft for leave game

--- src/assets/js/leave-game.js
+++ src/assets/js/leave-game.js
@@ -1,7 +1,29 @@
-document.querySelector("#leaveGame").addEventListener("click", () => {
-  window.location.href = "../index.html";
-});
+import * as CommunicationAbstractor from "./data-connector/api-communication-abstractor.js";
+import * as StorageAbstractor from "./data-connector/local-storage-abstractor.js";
 
-document.querySelector("#dontLeaveGame").addEventListener("click", () => {
+const gameId = StorageAbstractor.loadFromStorage("gameId");
+const playerName = StorageAbstractor.loadFromStorage("playerName");
+
+
+function initLeave(){
+  document.querySelector("#SettingsGame .leaveGame").addEventListener("click", deletePlayer);
+  document.querySelector("#dontLeaveGame").addEventListener("click", returnToGame);
+}
+
+function deletePlayer(){
+  CommunicationAbstractor.fetchFromServer(`/games/${gameId}/players/${playerName}`, "DELETE");
+}
+
+//document.querySelector("#leaveGame").addEventListener("click", goToIndex)
+/*
+function goToIndex() {
+  window.location.href = "index.html";
+}
+*/
+
+function returnToGame() {
   document.querySelector("#leave-game-option").classList.add("hidden");
-});
+}
+
+initLeave();
+export {initLeave, deletePlayer};
